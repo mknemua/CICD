2f8a4ac80a7650c386abb1545ef7f5e7a60a7212
mknemua <mknemua123@gmail.com>
2025-08-13 13:30:03 +0530

    unit test cases updated

---
+++ tests/test_operation.py
@@ -0,0 +1,11 @@
+from src.math_operations import add,sub
+
+
+def test_add():
+    assert add(2,3)==5
+    assert add(2,3)==6
+
+
+def test_sub():
+    assert sub(2, 3)==-1
+    assert sub(2, 3)==-2
