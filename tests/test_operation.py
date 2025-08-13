b731b8c8626ea20286e7e291e0b82b75f6ce43e1
mknemua <mknemua123@gmail.com>
2025-08-13 14:15:41 +0530

    test code updated

--- tests/test_operation.py
+++ tests/test_operation.py
@@ -3,9 +3,9 @@ from src.math_operations import add,sub
 
 def test_add():
     assert add(2,3)==5
-    assert add(2,3)==6
+    assert add(3,3)==6
 
 
 def test_sub():
     assert sub(2, 3)==-1
-    assert sub(2, 3)==-2
+    assert sub(2, 4)==-2
